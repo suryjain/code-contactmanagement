3fde31fb41b12153740ec21ca4f44f4f2d100445
surya <surya.jain@gmail.com>
2020-04-04 16:40:40 +0530

    Config server committed

---
+++ ConfigServer/src/test/java/com/evolenthealth/contactdetailsmanagement/configserver/ContactdetailsmanagementConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolenthealth.contactdetailsmanagement.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactdetailsmanagementConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
