cf10347814ade27cace180ef0c768e96b8d75265
surya <surya@DESKTOP-FSFQP5V>
2020-04-04 16:12:10 +0530

    Eureka server setup

---
+++ EurekaServer/src/test/java/com/evolenthealth/contactdetailsmanagement/eurekaserver/ContactdetailsmanagementEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolenthealth.contactdetailsmanagement.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactdetailsmanagementEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
