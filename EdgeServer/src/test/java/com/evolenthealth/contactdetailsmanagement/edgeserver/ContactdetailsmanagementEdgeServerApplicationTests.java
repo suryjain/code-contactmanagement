95615c8b7c72f7891bfea1f76442d05c2ad7367f
suryjain <surya.jain@gmail.com>
2020-04-04 19:12:15 +0530

    Edge Server committed

---
+++ EdgeServer/src/test/java/com/evolenthealth/contactdetailsmanagement/edgeserver/ContactdetailsmanagementEdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolenthealth.contactdetailsmanagement.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactdetailsmanagementEdgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
